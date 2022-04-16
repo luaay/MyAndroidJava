05a502092a0223aa9da79d445460fa607529af10
luaay <eng_luaay@yahoo.com>
2022-04-16 18:08:24 +0300

    update

--- app/src/main/java/com/hfad/mymessenger/MainActivity.java
+++ app/src/main/java/com/hfad/mymessenger/MainActivity.java
@@ -1,14 +1,26 @@
 package com.hfad.mymessenger;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onSendMessage(View view){
+
+        Intent intent = new Intent(this,ReceiveMessageActivity.class);
+
+        startActivity(intent);
+
+
     }
 }
---
+++ app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,15 @@
+package com.hfad.mymessenger;
+
+//import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ReceiveMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
