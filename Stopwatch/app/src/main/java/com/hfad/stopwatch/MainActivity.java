7c34516350e4a2ad697d147f053fef51a2313be6
luaay <eng_luaay@yahoo.com>
2022-04-17 08:19:46 +0300

    update

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.java
@@ -1,14 +1,30 @@
 package com.hfad.stopwatch;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+    public  void onClickStop(View view){
+        running = false;
+    }
+
+    public  void onClickReset(View view){
+        running = false;
+        seconds = 0;
+        
     }
 }
